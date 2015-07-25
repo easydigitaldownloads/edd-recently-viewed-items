4efa03dc69c8862b21feeb81a7a64c4524985ecb
John Parris <public@johnparris.com>
2015-07-24 23:57:52 -0400

    Add <strong> to settings heading.

--- includes/settings.php
+++ includes/settings.php
@@ -7,7 +7,7 @@ function edd_rvi_settings( $settings ) {
 	$rvi_settings = array(
 		array(
 			'id'   => 'edd_rvi_header',
-			'name' => __( 'Recently Viewed Items', 'edd-rvi' ),
+			'name' => '<strong>' . __( 'Recently Viewed Items', 'edd-rvi' ) . '</strong>',
 			'type' => 'header',
 			'size' => 'regular'
 		),
